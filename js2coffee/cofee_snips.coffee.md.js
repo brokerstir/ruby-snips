116b2db5285dd14562d1e40a1a98c594eecc4081
Robert Ris <take1risk@gmail.com>
2018-08-03 11:36:14 -0500

    add js2coffee folder

---
+++ js2coffee/cofee_snips.coffee.md.js
@@ -0,0 +1,15 @@
+var person = 
+	firstName:"John"
+	lastName:"Doe" 
+	age:50 
+	eyeColor:"blue"
+
+
+/////////////////////
+
+if time < 10
+	greeting = "Good Morning"
+else if time < 20
+	greeting = "Good Day"
+else 
+	greeting = "Good Evening"
---
+++ js2coffee/js_snips.js
@@ -0,0 +1,13 @@
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+//////////////////////
+
+if (time < 10) {
+    greeting = "Good morning";
+} else if (time < 20) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+
+//////////////////////
